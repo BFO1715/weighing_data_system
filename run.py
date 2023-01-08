84668d3fc35c244715f85ebc7e5228e775517145
BFO1715 <bethan@johnwhiteandson.com>
2023-01-08 19:14:05 +0000

    Validate data

--- run.py
+++ run.py
@@ -12,10 +12,27 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('WeighingData')
 
+
 def get_inweight():
     """
     Get inweight from user
     """
-    print("Please enter inweight values from the 5 vehicles, seperated by commas")
-    data_str = input("Enter value here: " )
-    print(f"You have entered {data_str} kg")
+    print("Please enter inweight values for 5 vehicles, seperated by commas.")
+    data_str = input("Enter value here: ")
+    
+    inweight = data_str.split(",")
+    validate_data(inweight)
+
+
+def validate_data(values):
+    """
+    Check values are intergers and 5 values given
+    """
+    try:
+        if len(values) != 5:
+            raise ValueError("Please enter 5 values")
+    except ValueError as e:
+        print(f"Invalid data: {e}, 1try again.")
+
+
+get_inweight()
